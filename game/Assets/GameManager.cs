9c037c3ef37fe35ed3b953ba626a125421078a00
Jaydin DaRosa <jaydin.darosa01@gmail.com>
2024-02-03 17:11:55 -0500

    Add game

---
+++ game/Assets/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager {
+    public static UnityWebRequest webSession = new UnityWebRequest();
+    
+    public static 
+
+
+}
