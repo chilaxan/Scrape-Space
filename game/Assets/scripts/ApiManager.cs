faa17220a9cadf2531b7e071acec085f609d0256
chilaxan <chilaxan@gmail.com>
2024-02-04 00:57:00 -0500

    add register function

--- game/Assets/scripts/ApiManager.cs
+++ game/Assets/scripts/ApiManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.Networking;
 public class ApiManager
 {
     public static string API = "https://scrape-space.tech/api/";
+
     public static IEnumerable login(string username, string password) {
         string data = JsonUtility.ToJson(new LoginUser(username, password));
         using (UnityWebRequest www = UnityWebRequest.Post(API + "login", data, "application/json")) {
@@ -15,10 +16,23 @@ public class ApiManager
                 Debug.Log(www.error);
             }
             else {
-                
+                Debug.Log('Logged In');
             }
         }
     }
     
+    public static IEnumerable register(string username, string password) {
+        string data = JsonUtility.ToJson(new LoginUser(username, password));
+        using (UnityWebRequest www = UnityWebRequest.Post(API + "register", data, "application/json")) {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success) {
+                Debug.Log(www.error);
+            }
+            else {
+                Debug.Log('Registered And Logged In');
+            }
+        }
+    }
     
 }
